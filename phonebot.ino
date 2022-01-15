ba00d043415f09314ca4791df9c96a520be58ca3
antfriend <antfriend@gmail.com>
2022-01-15 15:43:45 -0700

    added hardware servo driver

--- phonebot.ino
+++ phonebot.ino
@@ -1,29 +1,47 @@
 #include <Arduino.h>
-#include "ServoEasing.hpp"
+#include "HCPCA9685.h"
+ 
+#define  I2CAdd 0x40
+HCPCA9685 HCPCA9685(I2CAdd);
 
-ServoEasing Servo1;
+int kickstand_servo = 0;
+int left_servo = 1;
+int right_servo = 2;
 
-#define START_DEGREE_VALUE 90
+void park(){
+  HCPCA9685.Servo(kickstand_servo, 90);
+  HCPCA9685.Servo(left_servo, 90);
+  HCPCA9685.Servo(right_servo, 90);
+}
 
-//Servo myservo; 
-int myservo_pin = 5;
+void forward(){
+  HCPCA9685.Servo(left_servo, 200);
+  HCPCA9685.Servo(right_servo, 200);
+}
+
+void backward(){
+  HCPCA9685.Servo(left_servo, -10);
+  HCPCA9685.Servo(right_servo, -10);
+}
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
-  if (Servo1.attach(myservo_pin, START_DEGREE_VALUE) == INVALID_SERVO) {
-      //Serial.println(F("Error attaching servo"));
-  }
-
+  HCPCA9685.Init(SERVO_MODE);
+  HCPCA9685.Sleep(false);
+  park();
 }
 
 void loop() {
-  
   digitalWrite(LED_BUILTIN, HIGH);
-  //Servo1.setSpeed(5);
-  Servo1.easeTo(110);
-  delay(2000);
+  delay(200);
   digitalWrite(LED_BUILTIN, LOW);
-  //Servo1.setSpeed(20);
-  Servo1.easeTo(70);
-  delay(2000);
+  delay(200);  
+  forward();
+  delay(200);
+  digitalWrite(LED_BUILTIN, HIGH);
+  
+  delay(3000);
+  backward();
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(3000);
 }
